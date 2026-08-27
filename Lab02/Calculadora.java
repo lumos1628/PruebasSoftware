4664682d34e495e0b2ae9cdc311b6d6c44394e25
danielaapaza-stack <daniela.apaza@tecsup.edu.pe>
2026-08-26 20:51:30 -0500

    Agrega operacion de resta

--- Lab02/Calculadora.java
+++ Lab02/Calculadora.java
@@ -1,1 +1,9 @@
-public class Calculadora { public static void main(String[] args) { System.out.println("Hola"); } }
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("=== Calculadora ===");
+        System.out.println("Operaciones disponibles: suma");
+    }
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
