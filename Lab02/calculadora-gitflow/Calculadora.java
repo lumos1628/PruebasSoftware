74a940d6667d255d7b605841ca2b9b001248ab9f
scarnel <oscarquispe011@gmail.com>
2026-08-26 20:56:39 -0500

    Agrega operacion de multiplicacion

---
+++ Lab02/calculadora-gitflow/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("=== Calculadora ===");
+        System.out.println("Operaciones disponibles: suma");
+    }
+    public static int sumar(int a, int b) {
+        eturn a + b;
+    }
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
