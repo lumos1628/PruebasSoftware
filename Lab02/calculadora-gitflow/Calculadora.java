12328c388d01642ad605a23f89d931786d4c6ca7
gabriel-l <lumos2719@gmail.com>
2026-08-28 22:01:28 -0500

    feature resta

--- Lab02/calculadora-gitflow/Calculadora.java
+++ Lab02/calculadora-gitflow/Calculadora.java
@@ -9,4 +9,7 @@ public class Calculadora {
     public static int multiplicar(int a, int b) {
         return a * b;
     }
+    public static int restar(int a, int b) {
+        return a - b;
+    }
 }
